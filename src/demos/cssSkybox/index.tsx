df63855b90df07e5567d65731072aca74ec46b5b
sunnywindia <1603287239@qq.com>
2021-10-11 22:32:50 +0800

    feat:提交

--- src/demos/cssSkybox/index.tsx
+++ src/demos/cssSkybox/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import img1 from "../../images/front.png";
 import img2 from "../../images/back.png";
@@ -7,27 +7,40 @@ import img4 from "../../images/right.png";
 import img5 from "../../images/bottom.png";
 import img6 from "../../images/top.png";
 const CssSkyBox = () => {
-  const handleMouseDown = () => {
-    const e = window.event
-    console.log(e)
+  let lastX,lastY,currentX,currentY
+  const [roateData,setRotateData] = useState()
+  const handleMouseDown = (event:any) => {
+    const e = event||window.event
+    console.log(e,'按下')
+    lastX = e?.pageX
+    lastY = e?.pageY
+
   };
   const handleMouseMove = () => {
       const e = window.event
-      console.log(e)
+      // console.log(e)
   };
+  const handleMouseUp = (event:any) =>{
+    const e = event||window.event
+    currentX =e?.pageX
+    currentY = e?.pageY
+    console.log(e,'释放')
+  }
   return (
     <div className="camera" id="camera">
       <div
         className="space"
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        style={{transform:`${roateData}`}}
       >
-        <img className="front" src={img1} title="前" />
-        <img className="back" src={img2} title="后" />
-        <img className="left" src={img3} title="左" />
-        <img className="right" src={img4} title="右" />
-        <img className="bottom" src={img5} title="底" />
-        <img className="top" src={img6} title="上" />
+        <img className="front" src={img1} title="前" draggable="false"/>
+        <img className="back" src={img2} title="后"  draggable="false"/>
+        <img className="left" src={img3} title="左"  draggable="false"/>
+        <img className="right" src={img4} title="右" draggable="false"/>
+        <img className="bottom" src={img5} title="底" draggable="false"/>
+        <img className="top" src={img6} title="上" draggable="false"/>
       </div>
     </div>
   );
